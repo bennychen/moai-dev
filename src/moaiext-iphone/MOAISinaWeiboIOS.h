f33cfa9f2b2e476e0225abfdfd7d2d6a66d09e80
Benny <rockerbenny@gmail.com>
2012-11-05 13:24:22 +0800

    Added copyright header

--- src/moaiext-iphone/MOAISinaWeiboIOS.h
+++ src/moaiext-iphone/MOAISinaWeiboIOS.h
@@ -1,8 +1,35 @@
-// Copyright (c) 2010-2011 Zipline Games, Inc. All Rights Reserved.
-// http://getmoai.com
+//
+//  MOAISinaWeiboIOS.h
+//  Sina web support for MOAI SDK (iOS only)
+//
+//  Created by Benny Chen (rockerbenny@gmail.com) on 11/5/12.
+//
+//// Example usage:
+//
+//
+// MIT License
+// Copyright (C) 2012. Benny Chen
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
 #ifndef MOAISINAWEIBOIOS_H
 #define MOAISINAWEIBOIOS_H
+
+#ifndef DISABLE_SINAWEIBO
 
 #include <moaicore/moaicore.h>
 
@@ -28,5 +55,7 @@ public:
 					~MOAISinaWeiboIOS			();
 	void			RegisterLuaClass	( MOAILuaState& state );
 };
+
+#endif
 
 #endif
